c447997477cef636d0871264454cbb4a47ccfeef
Tarcisio Coutinho <tcs5cin@gmail.com>
2023-01-31 14:15:32 -0300

    common domain

---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/Money.java
@@ -0,0 +1,30 @@
+package com.food.ordering.system.domain.valueobject;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
+public record Money(BigDecimal amount) {
+    private static final MathContext mathContext = new MathContext(2, RoundingMode.HALF_EVEN);
+
+    public boolean isGreaterThanZero() {
+        return amount != null && amount.compareTo(BigDecimal.ZERO) > 0;
+    }
+
+    public boolean isGreaterThan(Money money) {
+        return amount != null && amount.compareTo(money.amount()) > 0;
+    }
+
+    public Money add(Money money) {
+        return new Money(this.amount.add(money.amount(), mathContext));
+    }
+
+    public Money subtract(Money money) {
+        return new Money(this.amount.subtract(money.amount(), mathContext));
+    }
+
+    public Money multiply(int multiplier) {
+        return new Money(this.amount.multiply(new BigDecimal(multiplier), mathContext));
+    }
+
+}
---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/OrderStaus.java
@@ -0,0 +1,5 @@
+package com.food.ordering.system.domain.valueobject;
+
+public enum OrderStaus {
+    PENDING, PAID, APPROVED, CANCELLING, CANCELLED
+}
