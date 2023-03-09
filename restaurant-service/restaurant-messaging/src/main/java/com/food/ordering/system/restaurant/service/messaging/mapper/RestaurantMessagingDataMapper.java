db3641a6b232bdaed1f48417f2e3a7b955914402
Tarcisio Coutinho <tcs5cin@gmail.com>
2023-03-09 19:03:27 -0300

    restaurant service

---
+++ restaurant-service/restaurant-messaging/src/main/java/com/food/ordering/system/restaurant/service/messaging/mapper/RestaurantMessagingDataMapper.java
@@ -0,0 +1,68 @@
+package com.food.ordering.system.restaurant.service.messaging.mapper;
+
+import com.food.ordering.system.domain.valueobject.ProductId;
+import com.food.ordering.system.domain.valueobject.RestaurantOrderStatus;
+import com.food.ordering.system.kafka.order.avro.model.OrderApprovalStatus;
+import com.food.ordering.system.kafka.order.avro.model.RestaurantApprovalRequestAvroModel;
+import com.food.ordering.system.kafka.order.avro.model.RestaurantApprovalResponseAvroModel;
+import com.food.ordering.system.restaurant.service.domain.dto.RestaurantApprovalRequest;
+import com.food.ordering.system.restaurant.service.domain.entity.Product;
+import com.food.ordering.system.restaurant.service.domain.event.OrderApprovedEvent;
+import com.food.ordering.system.restaurant.service.domain.event.OrderRejectedEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Component
+public class RestaurantMessagingDataMapper {
+    public RestaurantApprovalResponseAvroModel
+    orderApprovedEventToRestaurantApprovalResponseAvroModel(OrderApprovedEvent orderApprovedEvent) {
+        return RestaurantApprovalResponseAvroModel.newBuilder()
+                .setId(UUID.randomUUID().toString())
+                .setSagaId("")
+                .setOrderId(orderApprovedEvent.orderApproval().getOrderId().value().toString())
+                .setRestaurantId(orderApprovedEvent.restaurantId().value().toString())
+                .setCreatedAt(orderApprovedEvent.createdAt().toInstant())
+                .setOrderApprovalStatus(OrderApprovalStatus.valueOf(orderApprovedEvent.
+                        orderApproval().getApprovalStatus().name()))
+                .setFailureMessages(orderApprovedEvent.failureMessages())
+                .build();
+    }
+
+    public RestaurantApprovalResponseAvroModel
+    orderRejectedEventToRestaurantApprovalResponseAvroModel(OrderRejectedEvent orderRejectedEvent) {
+        return RestaurantApprovalResponseAvroModel.newBuilder()
+                .setId(UUID.randomUUID().toString())
+                .setSagaId("")
+                .setOrderId(orderRejectedEvent.orderApproval().getOrderId().value().toString())
+                .setRestaurantId(orderRejectedEvent.restaurantId().value().toString())
+                .setCreatedAt(orderRejectedEvent.createdAt().toInstant())
+                .setOrderApprovalStatus(OrderApprovalStatus.valueOf(orderRejectedEvent.
+                        orderApproval().getApprovalStatus().name()))
+                .setFailureMessages(orderRejectedEvent.failureMessages())
+                .build();
+    }
+
+    public RestaurantApprovalRequest
+    restaurantApprovalRequestAvroModelToRestaurantApproval(RestaurantApprovalRequestAvroModel
+                                                                   restaurantApprovalRequestAvroModel) {
+        return RestaurantApprovalRequest.builder()
+                .id(restaurantApprovalRequestAvroModel.getId())
+                .sagaId(restaurantApprovalRequestAvroModel.getSagaId())
+                .restaurantId(restaurantApprovalRequestAvroModel.getRestaurantId())
+                .orderId(restaurantApprovalRequestAvroModel.getOrderId())
+                .restaurantOrderStatus(RestaurantOrderStatus.valueOf(restaurantApprovalRequestAvroModel
+                        .getRestaurantOrderStatus().name()))
+                .products(restaurantApprovalRequestAvroModel.getProducts()
+                        .stream().map(avroModel ->
+                                Product.builder()
+                                        .id(new ProductId(UUID.fromString(avroModel.getId())))
+                                        .quantity(avroModel.getQuantity())
+                                        .build())
+                        .collect(Collectors.toList()))
+                .price(restaurantApprovalRequestAvroModel.getPrice())
+                .createdAt(restaurantApprovalRequestAvroModel.getCreatedAt())
+                .build();
+    }
+}
